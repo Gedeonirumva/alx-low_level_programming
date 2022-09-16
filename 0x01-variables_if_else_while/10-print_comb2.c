7c36427df57869c81d91f23554f457b4476e61c0
Gedeonirumva <84919556+Gedeonirumva@users.noreply.github.com>
2022-09-16 22:43:16 +0200

    Printcomb2

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,26 +1,26 @@
 #include <stdio.h> 
- #include <stdlib.h> 
- #include <time.h> 
- /** 
-  * main - Entry point 
-  * 
-  * Return: Always 0 (Success) 
-  */ 
- int main(void) 
- { 
-         int d; 
-  
-         for (d = 0; d < 100; d++) 
-         { 
-                 putchar((d / 10) + '0'); 
-                 putchar((d % 10) + '0'); 
-                 if (d != 99) 
-                 { 
-                         putchar(','); 
-                         putchar(' '); 
-                 } 
-         } 
-         putchar('\n'); 
-  
-         return (0); 
- }
+/** 
+* main - Prints the numbers from 00 to 99 
+* 
+* Return: Always (Success) 
+*/ 
+int main(void) 
+{ 
+int c, i; 
+c = i = '0'; 
+for (c = '0'; c <= '9'; c++) 
+{ 
+for (i = '0'; i <= '9'; i++) 
+{ 
+putchar(c); 
+putchar(i); 
+if ((c != '9') || (c == '9' && i != '9'))
+{ 
+putchar(','); 
+putchar(' '); 
+} 
+} 
+} 
+putchar('\n'); 
+return (0); 
+}
