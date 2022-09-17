b9c590c4e0fb61c4bde865e33d9591e9c79d4491
Gedeonirumva <84919556+Gedeonirumva@users.noreply.github.com>
2022-09-17 04:55:13 +0200

    102-printcomb task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,9 @@
 #include <stdio.h> 
 /** 
-* main - Prints 3 combination of numbers 
-* 
-* Return: Always (Success) 
-*/ 
+ * main - Prints 3 combination of numbers 
+ * 
+ * Return: Always (Success) 
+ */ 
 int main(void) 
 { 
 int c, i, k, j; 
