ea17ac8dd7e7a349dc0d4d1539de79bd621e242b
Gedeonirumva <84919556+Gedeonirumva@users.noreply.github.com>
2022-09-17 06:49:24 +0200

    Task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,8 @@
 * 
 * Return: Always 0 (Success) 
 */
-int main(void) 
+int main(void)
 { 
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61); 
-  
-    return (1); 
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61); 
+return (1);
 }
