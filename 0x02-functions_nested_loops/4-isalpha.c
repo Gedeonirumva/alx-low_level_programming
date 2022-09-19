1c90bd96c833b2fbde1a372626966228f244dd31
Gedeonirumva <gedirum100@gmail.com>
2022-09-19 09:42:40 -0700

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,15 @@
-#include "holberton.h" 
-  
- /** 
-  * _isalpha - check for lower case letter 
-  * @c : character to check 
-  * Return:0 or 1 
-  */ 
-  
- int _isalpha(int c) 
- { 
-         return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90)); 
-  
- }
+#include "main.h"
+
+/**
+ * _isalpha - check the code for Holberton School students.
+ * @c: letter being tested
+ * Return: Always 0
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
