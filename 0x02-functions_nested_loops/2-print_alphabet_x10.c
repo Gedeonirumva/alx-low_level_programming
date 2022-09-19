bf8f7c21c48429080d7b2d824ad6692441511241
Gedeonirumva <gedirum100@gmail.com>
2022-09-19 09:34:05 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,20 @@
-#include "holberton.h" 
-  
- /** 
-  * print_alphabet_x10 - prints alphabet 10 times 
-  * Return:void 
-  */ 
-  
- void print_alphabet_x10(void) 
- { 
-         char c; 
-         int i = 0; 
-  
-         while (i < 10) 
-         { 
-                 c = 'a'; 
-                 while (c <= 'z') 
-                 { 
-                         _putchar(c); 
-                         c++; 
-                 } 
-                 _putchar('\n'); 
-                 i++; 
-         } 
-  
- }
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  prints the alphabet x10
+ */
+
+void print_alphabet_x10(void)
+{
+int b;
+int b2;
+
+for (b = 0; b <= 9; b++)
+{
+for (b2 = 'a'; b2 <= 'z'; b2++)
+{
+_putchar(b2);
+																	}
+_putchar('\n');
+}
+}
